4e93897b268517e186a2b737b08d323026b350a8
ttj <1069696203@qq.com>
2023-06-14 18:52:23 +0800

    two commit

--- src/main/java/com/ttj/GitTestApplication.java
+++ src/main/java/com/ttj/GitTestApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitTestApplication {
 
     public static void main(String[] args) {
+        System.out.println("修改版本之后");
         SpringApplication.run(GitTestApplication.class, args);
     }
 
